c1e0dfa14285ab34ab0846bf43f3b0f07c653e97
caixinxin <caixx@hadlinks.com>
2017-08-25 17:55:24 +0800

    update nav menu

--- js/views/getNav.js
+++ js/views/getNav.js
@@ -1,9 +1,13 @@
 /*created by caixx on 20170823*/
 $(function(){
+	var data2;
+	var initNavZtree = function(data1){
+			var zNodes,
+				zNodes_per = [];
 
-	var initNavZtree = function(data){
-			var zNodes= [];
-
+			zNodes = data1;
+			console.log(data1,data2)
+			
 			
 		    //ajax获取数据 模拟数据
 		    var setting = {
@@ -23,38 +27,39 @@ $(function(){
 						rootPId: 0
 					},
 					key:{
-						name:"name",
-						link:"link"
+						name:"name"
 					}
 				},
+				
 				callback:{
 					onClick:onClick
 				}
 		    };
 
-		    
 		   
 		    function onClick(e,treeId, treeNode) {
-		     	var zTree = $.fn.zTree.getZTreeObj("leftTreeNav");		
-				zTree.expandNode(treeNode);
+		     	var treeObj = $.fn.zTree.getZTreeObj("leftTreeNav");		
+				treeObj.expandNode(treeNode);
 
 			}
 
 			/*自定义一级导航icon*/
 			function addDiyDom(treeId, treeNode){
+				
+
 				if (treeNode.parentNode && treeNode.parentNode.id!=0) return;
 				console.log(treeNode)
 				var aObj = $("#" + treeNode.tId + '_a');
 				var bObj = $("#" + treeNode.tId + '_a.level0')
 
 
+
 				bObj.each(function(){
 					var iconHtml = '<i class="icon-star"></i>';
 					$(this).prepend(iconHtml)
-					//console.log($(this).parent())
 				})
 				
-				console.log(bObj)
+				//console.log(bObj)
 				//console.log(treeNode.children())
 				//console.log(treeNode,treeNode.id)
 				if(treeNode.id != undefined && treeNode.id !=null){
@@ -70,30 +75,29 @@ $(function(){
 
 			
 
-			zNodes = data;
+			
 			
 			$(document).ready(function(){
 				
 
 		    	var treeObj = $.fn.zTree.init($('#leftTreeNav'), setting, zNodes);
 
-		    	var firstNodes = treeObj.getNodesByParam("level","1");
-		    	// 从树对象中获取一级导航的code 创建url字符串
-		    	var urls = $.map(firstNodes,function(item){
-		    		if( item.code != undefined){
+		    	var firstNodes = treeObj.getNodesByParam("level","0");
+		    	
+				var secondNodes = treeObj.getNodesByParam("level","1");
+				// 从树对象中获取一级导航的code 创建url字符串
+		    	// var urls = $.map(secondNodes,function(item){
 
-		    			return item.code+".html"		    		
-		    		}
-		    	})
+		    	// 	if( item.code != undefined){
+		    	// 		return item.code+".html"		    		
+		    	// 	}
+		    	// })
 
 
-		    	console.log(firstNodes)
-		    	console.log(urls)
+				console.log(secondNodes)
+					
 
-		    	//获取第三级的功能子节点 并隐藏
-		    	var secondNodes = treeObj.getNodesByParam("level", "2");
-		    	//console.log(nodes)
-		    	treeObj.hideNodes(secondNodes);
+		    		
 		    	
 		    })
 
@@ -101,21 +105,43 @@ $(function(){
 		
 	};
 
+	/*获取导航树*/
 	function getNav(){
 		$.ajax({
 			url:'resources/json/listNavTree.json',
 			dataType:"json",
 			//async : false,
-			success:function(data){
+			success:function(res){
 				console.log('初始化菜单-----')
-				console.log(data);
-				var data = data.data;				
+				console.log(res);
+				var data = res.data;				
 				//本地存储导航数据
 				setStorage("nav",data);
 				judgeStorage('nav');
 			},
 			error:function(){
 				console.log('左侧导航------后台出错');
+			}
+
+			
+		})
+	}
+	/*获取功能权限树*/
+	function getPermission(){
+		$.ajax({
+			url:'resources/json/listPermission.json',
+			dataType:"json",
+			//async : false,
+			success:function(res){
+				console.log('初始化功能权限-----')
+				console.log(res);
+				var data = res.data;				
+				//本地存储导航数据
+				setStorage("permission",data);
+				judgeStorage('permission');
+			},
+			error:function(){
+				console.log('功能权限------后台出错');
 			}
 
 			
@@ -128,28 +154,40 @@ $(function(){
 	var judgeStorage = function (name){
 
 		if(name == "nav"){
-				if(getStorage(name)!= undefined){
-	    			var data1 = getStorage("nav");
-	    	 		
-	    	 		initNavZtree(JSON.parse(data1));
+			if(getStorage(name)!= undefined){
+    			 var data1 = getStorage("nav");
+    	 		
+    	 		initNavZtree(JSON.parse(data1));
 
-				}else{
-				 	getNav();
-				 
-				}
-		    }
-			
-		   	if(name == "username"){
-				if(getStorage(name)!= undefined && getStorage(name)!= null){
-	    			var data2 = getStorage("username");
-	    		 	$('.user.user-menu .user-name').html(data2);
-	    		 	
-				}else{
-			 		publicFun.getUserInfo();
-				} 
-		    }
+			}else{
+			 	getNav();
+			 
+			}
+	    }
+		
+	    if(name == "permission"){
+	    	if(getStorage(name)!= undefined){
+    		   data2 = getStorage("permission");	
+    	 		//initNavZtree(JSON.parse(data2));
+
+			}else{
+			 	getPermission();
+			 
+			}
+	    }
+
+	   	if(name == "username"){
+			if(getStorage(name)!= undefined && getStorage(name)!= null){
+    			var data2 = getStorage("username");
+    		 	$('.user.user-menu .user-name').html(data2);
+    		 	
+			}else{
+		 		publicFun.getUserInfo();
+			} 
+	    }
 	};
 	judgeStorage("nav");
+	judgeStorage("permission");
 
 	/*写入本地存储数据
 	* @param name  命名
@@ -159,12 +197,12 @@ $(function(){
 		//判断json结构不严谨 
 		var value = typeof(data) == "object" ? JSON.stringify(data):data;
 
-		console.log('set sessionstorage----')
+		console.log('set localStorage----')
 		//console.log(typeof(value))
-		if(window.sessionStorage){
-			sessionStorage.setItem(name,value);	
+		if(window.localStorage){
+			localStorage.setItem(name,value);	
 		}else{
-			alert('浏览器不支持sessionStorage');
+			alert('浏览器不支持localStorage');
 			return
 		}	
 	}
@@ -173,16 +211,31 @@ $(function(){
 	*/
 	function getStorage(name){
 
-		if(! window.sessionStorage){
-			alert('浏览器不支持sessionStorage');
+		if(! window.localStorage){
+			alert('浏览器不支持localStorage');
 			return	
 		}
-		if(sessionStorage.length>0 && sessionStorage.getItem(name) ){
-			var value = sessionStorage.getItem(name);
+		if(localStorage.length>0 && localStorage.getItem(name) ){
+			var value = localStorage.getItem(name);
 			//console.log('get---'+typeof(value))
 			//console.log(value)
 			return value;
 		}
+	}
+
+	//localStorage.clear();
+	/*判断当前页面刷新或离开或关闭*/
+	window.onbeforeunload = function(){
+		alert(1)
+		BootstrapDialog.confirm({
+			title:"提示",
+			type:BootstrapDialog.TYPE_DANGER,
+			size: BootstrapDialog.SIZE_SMALL,
+			message:"确定离开此页面吗？",
+			callback:function(res){
+
+			}
+		});	
 	}
 
 })
